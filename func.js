c5cf5a741b69761e4a4b84ab9910d297cda2383b
Karan Singh <karanrsingh2662006@gmail.com>
2022-09-24 20:38:50 +0530

    new version

--- func.js
+++ func.js
@@ -2,38 +2,46 @@
 
 let controls = document.querySelectorAll(".controls");
 let output = document.querySelector(".output");
+let info = document.querySelector(".info");
 Array.from(controls).forEach((controls) => {
   controls.addEventListener("click", (e) => {
-      switch (e.target.innerHTML) {
+
+      switch (e.target.innerText) {
           case "C":
-              output.innerHTML = "";
+              output.innerText = "";
         break;
 
       case "Del":
-        if (output.innerHTML == "Error!!!") {
-          output.innerHTML = "";
+        if (output.innerText == "Error!!!") {
+          output.innerText = "";
         }
-        output.innerHTML = output.innerHTML.slice(0, -1);
+        output.innerText = output.innerText.slice(0, -1);
         
         break;
         
         case "=":
         try {
-            output.innerHTML = eval(output.innerHTML);
+            output.innerText = eval(output.innerText);
         } catch {
             // str = eval(output.innerHTML);
-            output.innerHTML = "Error!!!";
+            output.innerText = "Error!!!";
         }
         
         break;
 
         default:
             if (output.innerHTML == "0") {
-                output.innerHTML = e.target.innerHTML;
+                output.innerHTML = e.target.innerText;
             } else {
-                output.innerHTML += e.target.innerHTML;
+                output.innerHTML += e.target.innerText;
             }
             break;
         }
   });
 });
+
+
+function about() {
+    confirm("This is a calculator app made by Karan Singh")
+    return
+}
